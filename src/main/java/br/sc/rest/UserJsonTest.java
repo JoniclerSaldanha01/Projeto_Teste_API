c18216d80d4dac856c2defde629fc793b9562922
Automacao_Levv <jonicler.isaias.saldanha@gmail.com>
2023-05-13 10:36:13 -0300

    Commit atualizacao curso

--- src/main/java/br/sc/rest/UserJsonTest.java
+++ src/main/java/br/sc/rest/UserJsonTest.java
@@ -10,6 +10,9 @@ import org.junit.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 
 public class UserJsonTest {
 
@@ -43,5 +46,57 @@ public class UserJsonTest {
 		//from
 		int id = JsonPath.from(response.asString()).getInt("id");
 		Assert.assertEquals(1, id);
+	}
+
+	@Test
+	public void segundoNivel(){
+		given()
+				.when()
+				.get("https://restapi.wcaquino.me/users/2")
+				.then()
+				.statusCode(200)
+				.body("name", containsString("Joaquina"))
+				.body("endereco.rua", is("Rua dos bobos"));
+	}
+
+	@Test
+	public void terceiroNivel(){
+		given()
+				.when()
+				.get("https://restapi.wcaquino.me/users/3")
+				.then()
+				.statusCode(200)
+				.body("name", containsString("Ana"))
+				.body("filhos", hasSize(2))
+				.body("filhos[0].name", is("Zezinho"))
+				.body("filhos[1].name", is("Luizinho"))
+				.body("filhos.name", hasItem("Zezinho"))
+				.body("filhos.name", hasItems("Zezinho", "Luizinho"))
+		;
+	}
+
+	@Test
+	public void erroUsuarioInexistente(){
+		given()
+				.when()
+				.get("https://restapi.wcaquino.me/users/4")
+				.then()
+				.statusCode(404)
+				.body("error", is("Usuário inexistente"))
+				;
+	}
+
+	@Test
+	public void listaRaiz(){
+		given()
+				.when()
+				.get("https://restapi.wcaquino.me/users")
+				.then()
+				.statusCode(200)
+				.body("", hasSize(3))
+				.body("name", hasItems("João da Silva", "Maria Joaquina", "Ana Júlia"))
+				.body("age[1]", is(25))
+				.body("filhos.name", hasItem(Arrays.asList("Zezinho", "Luizinho")))
+		;
 	}
 }
